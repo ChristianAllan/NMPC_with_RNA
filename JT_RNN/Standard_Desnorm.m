a01dd1454f9c06dad122ee69a437a27c33f518ef
Allan <acbalves.ege@uesc.br>
2026-04-28 18:16:37 -0300

    ONNX

--- JT_RNN/Standard_Desnorm.m
+++
@@ -1,3 +0,0 @@
-function Y = Standard_Desnorm(Y_norm, meanY, stdY)
-    Y = Y_norm .* stdY + meanY;
-end
--- JT_RNN/Standard_Norm.m
+++
@@ -1,3 +0,0 @@
-function X_norm = Standard_Norm(X, meanX, stdX)
-    X_norm = (X - meanX) ./ stdX;
-end
